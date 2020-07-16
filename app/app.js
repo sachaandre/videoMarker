95a8b8a4c1150cb8fa514c20a3495a3187255634
Sacha Andre <andresachaa@gmail.com>
2020-07-16 14:35:19 +0200

    Adding clickable markers

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 //import Router from './modules/router.js';
-import {player} from './views/player.js';
+import {player, generatePlayerPage} from './views/player.js';
 //
 const routes = {
   '/' : home,
@@ -8,6 +8,7 @@ const routes = {
 
 const mainDiv = document.getElementById('main');
 mainDiv.innerHTML = routes[window.location.pathname].outerHTML;
+if (window.location.pathname == '/player') generatePlayerPage();
 
 const onNavigate = (pathname) => {
   window.history.pushState(
--- app/views/player.js
+++ app/views/player.js
@@ -2,36 +2,36 @@
 import {MarkedVid} from '/lib/videoMarker/MarkedVid.js';
 
 //Creating player page container (w/ id)
-let player = document.createElement("div");
+export let player = document.createElement("div");
 player.setAttribute("id","playerContainer");
 
-//Creating a MarkedVid instance
-let markedVid = new MarkedVid("/media/module_1/test.mp4", "mp4");
+//Generating after changing page
+function generatePlayerPage(){
+  let player = document.getElementById("playerContainer");
+  //Creating a MarkedVid instance
+  let markedVid = new MarkedVid("/media/module_1/test.mp4", "mp4");
 
-//Pushing markers
-markedVid.pushMarker("00:00","Debut");
-markedVid.pushMarker("00:29","Contexte");
-markedVid.pushMarker("01:35","Axe Hierarchique");
+  //Pushing markers
+  markedVid.pushMarker("00:00","Debut");
+  markedVid.pushMarker("00:29","Contexte");
+  markedVid.pushMarker("01:35","Axe Hierarchique");
 
-//Creating a container for video player, buttons and mark list
-let vidContainer = document.createElement("div");
-vidContainer.setAttribute("id","videoContainer");
-player.appendChild(vidContainer);
+  //Creating a container for video player, buttons and mark list
+  let vidContainer = document.createElement("div");
+  vidContainer.setAttribute("id","videoContainer");
+  player.appendChild(vidContainer);
 
-let markListContainer = document.createElement("div");
-markListContainer.setAttribute("id","markListContainer");
-player.appendChild(markListContainer);
+  let markListContainer = document.createElement("div");
+  markListContainer.setAttribute("id","markListContainer");
+  player.appendChild(markListContainer);
 
-//Generating the video player
-markedVid.generateVideoPlayer(vidContainer);
+  //Generating the video player
+  markedVid.generateVideoPlayer(vidContainer);
 
-//Generating marks's list
-console.log(markedVid.marksArray);
-markedVid.generateMarkerList(markListContainer);
+  //Generating marks's list
+  markedVid.generateMarkerList(markListContainer);
 
+}
 
 //end of file
-if (player.innerHTML == null || player.innerHTML == ""){
-  player.innerHTML = `<p> pas de vidéo à afficher </p>`
-}
-export {player};
+export { generatePlayerPage };
